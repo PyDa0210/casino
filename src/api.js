54fe0b7e56e62aa4b671f4e81792c3195a423bea
PyDa0210 <danieloyola0210@gmail.com>
2024-11-29 12:33:12 -0500

    Primer commit en el nuevo repositorio

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Configuración base de Axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Cambia el puerto si es diferente
+  baseURL: process.env.REACT_APP_BACKEND_URL, // Usamos la variable de entorno
   withCredentials: true, // Permite el envío de cookies (si es necesario para la autenticación)
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`, // Token JWT del usuario autenticado
