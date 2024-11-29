604d555e75fd0ec7b88a7fe4663b1a63cb95ca93
PyDa0210 <danieloyola0210@gmail.com>
2024-11-29 12:54:02 -0500

    Agregado el script start

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 //src/app.js
-//importar dependencias de funcionamiento.
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
-//importar *file routes
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import footballRoutes from './routes/football.routes.js';
@@ -13,21 +11,23 @@ import betRoutes from './routes/bet.routes.js';
 import dashboardRoutes from './routes/dashboard.routes.js';
 import transactionRoutes from './routes/transaction.routes.js';
 
+const app = express();
 
-const app = express()
+// CORS Configuración para producción y desarrollo
+const allowedOrigins = process.env.NODE_ENV === 'production' ? ['https://uscoplay.netlify.app'] : ['http://localhost:5173'];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,  // Permite enviar cookies y cabeceras con el token
+}));
 
 // Middlewares globales
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(errorHandler);
-app.use(cors({
-    origin: 'http://localhost:5173', // O 'http://localhost:5173' se modifica dependiento del port de front
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-}));
-
 
 // Rutas
 app.use('/api/users', userRoutes);
@@ -36,6 +36,5 @@ app.use('/api/foot', footballRoutes);
 app.use('/api/bets', betRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/transactions', transactionRoutes);
-
 
 export default app;
