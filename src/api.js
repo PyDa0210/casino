60ac6fcf4c78b677917ccdfceedf9f434ce2c229
PyDa0210 <danieloyola0210@gmail.com>
2024-11-29 13:22:24 -0500

    si

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
 // src/api.js
 import axios from 'axios';
 
-// Configuración base de Axios
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api', // Usamos la variable de entorno, o localhost para desarrollo
+  baseURL: 'https://casino-q59h.onrender.com/api', // URL del backend en Render
   withCredentials: true, // Permite el envío de cookies (si es necesario para la autenticación)
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`, // Token JWT del usuario autenticado
